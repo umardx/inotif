83393970301ee5bb9308b3aabe5265a58339d548
Umar Nawawi <umardx@s.itb.ac.id>
2017-08-14 20:02:21 +0700

    Update supervisord-compile.sh
    Append `inotif_enable="YES"` if not exist.

--- ansible-playbook/supervisord-compile.sh
+++ ansible-playbook/supervisord-compile.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 cd /usr/ports/sysutils/py-supervisor
+# Compile default option.
 make install clean BATCH=yes
+# Append `inotif_enable="YES"` if not exist.
+grep -q -F 'inotif_enable="YES"' /usr/local/etc/supervisord.conf || echo 'inotif_enable="YES"' >> /usr/local/etc/supervisord.conf
