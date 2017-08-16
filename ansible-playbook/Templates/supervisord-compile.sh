50f4d90a974441620653f9f4bb0ccf56203bba32
Ardi <ardi.nusawan13@gmail.com>
2017-08-16 13:27:35 +0700

    Debian and FreeBSD done

--- ansible-playbook/Templates/supervisord-compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd /usr/ports/sysutils/py-supervisor
-# Compile default option.
-make install clean BATCH=YES
-# Append `inotif_enable="YES"` if not exist.
-grep -q -F 'inotif_enable="YES"' /etc/rc.conf || echo 'inotif_enable="YES"' >> /etc/rc.conf
